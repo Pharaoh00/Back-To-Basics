bc65891695f3f1b82fd17cecd6d92c71839be853
Pharaoh <hamoncsl@hotmail.com>
2018-12-17 13:53:53 -0200

    Add files via upload

---
+++ Exercise/exercise1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+# Exercise from Control Flow
+# Write a program that prints the numbers from 1 to 100.
+# But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz".
+# For numbers which are multiples of both three and five print "FizzBuzz".
+
+# Tip:
+# For check if a number is a multiple of something you need to use the
+# modulus from math. This mean the factorization.
+# This this example:
+# print(5 % 5) # output: 0
+# print(15 % 5) # output: 0
+# print(26 % 3) # output: 2
+# So for you know if a number is a multiple of something
+# you need to test them and if is 0 the number is a multiple.
+#
+# GOOD LUCK
