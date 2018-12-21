03b248af7a7ef40708daa71ed88839e0d3a0cb9e
Pharaoh <hamoncsl@hotmail.com>
2018-12-21 17:06:28 -0200

    Add files via upload

--- Exercise/examples/example1.py
+++ Exercise/examples/example1.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
+# ----------------------------------------------------------- #
+# Filename: test.py
+# Creation Date: sexta-feira, 21 dezembro 2018 04:40
+# Author: Hamon-Rá Taveira Guimarães
+# Contact:
+#         GitHub: https://github.com/
+#         E-mail: hamoncsl@hotmail.com
+#         Facebook: https://www.facebook.com/hamonra.taveira
+# ----------------------------------------------------------- #
 
 class Player:
     
@@ -12,38 +21,28 @@ class Player:
         self.health = health
         self.mana = mana
         self.is_friend = is_friend
+        self.damage = 0 # Just a way to output this damage
 
     def __eq__(self, other):
-        # This is a magic method
-        # Dont worry to much about it right now.
-        # If you want, you can check this site:
-        # https://rszalski.github.io/magicmethods/
-        #
-        # This mean if foo == bar:
-        # __eq__ is the "==" operation
-        #
-        # if self.is_friend is True and other.is_friend is True:
-        #     return True
-        # elif self.is_friend is True and other.is_friend is False:
-        #     return False
 
         if self.is_friend and other.is_friend:
-            # if both is True, return True
             return True
         elif not self.is_friend or not other.is_friend:
-            # if one of them is False, return False
             return False
 
     def battle(self, defensor):
-        damage = self.atack_p - defensor.defense_p
-        defensor.health -= damage
+        self.damage = self.atack_p - defensor.defense_p
+        defensor.health -= self.damage
         
 p1 = Player("Pharaoh", "Warrior", 10, 5)
 p2 = Player("Skeleton", "Necromancer", 5, 2, mana=500, is_friend=False)
 
-while p1.health >= 0 or p2.health >= 0:
-    p1.battle(p2)
-    p1.battle(p2)
-    print(p2.health)
-    if p1.health <= 0 or p2.health <= 0:
-        break
+if not p1 == p2: # Dump check for hostilite
+    print("You encounter a Enemy!")
+    print("Enemy {} have {} of life.".format(p2.name, p2.health))
+    while p1.health >= 0 or p2.health >= 0:
+        p1.battle(p2)
+        print("Your damage are: {} - Your enemy life: {}"
+              .format(p1.damage, p2.health))
+        if p1.health <= 0 or p2.health <= 0:
+            break
