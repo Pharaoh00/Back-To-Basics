5123f87457e5435ba08fd585d95ceca2ba43b43c
Pharaoh <hamoncsl@hotmail.com>
2018-12-21 17:17:14 -0200

    Add files via upload

---
+++ Exercise/exercise2.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+# Exercise from Class's
+#
+# Write a game which have a main loop.
+# Make a text "turn based" game.
+# Make a menu options for the combat.
+# Make a proper system of atack and defense points. Which have equipaments.
+#
+# Make a system with magic, which uses mana.
+#
+# Make a proper system for handle the user input, in combat, like:
+# Ask whats the user want to do on battle, example:
+# "Skeleton atack you!"
+# "Options: Atack - Magic - Potions - PASS TURN"
+# Use json files for keep your informations about:
+# Player name, last names, guild, atack/defense points, equipaments and etc
+# for Enemys too.
+#
+# You dont need the game to be a class. If you want... you can make a class
+# but dont need.
+# Make at least five enemys.
+# Make at least three types of equipaments.
+# Dont need to use __magic__ methods. Only if you want.
+# 
+# RULES:
+# If you want, you can search on google.
+# Be creative.
+# Have fun.
+#
+# GOOD LUCK!
